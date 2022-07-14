17f568aa5ba2a0e1af1bc012f0795df919f36c23
eggplantiny <eggplantiny@gmail.com>
2022-07-15 03:37:19 +0900

    feat: add ToC Component

--- components/organisms/TableOfContents.vue
+++ components/organisms/TableOfContents.vue
@@ -1,11 +1,23 @@
-<template>
-    <div>
-        Table Of Contents {{ data }}
-    </div>
-</template>
 <script lang="ts" setup>
-const data = ref('hello world')
-</script>
-<style scoped lang="scss">
+import { onMounted, ref, useContent } from '#imports'
 
-</style>
+const { toc } = useContent()
+</script>
+
+<template>
+  <div class="py-8 lg:py-12 top-8 left-0 sticky">
+    <div class="text-xl font-bold">
+      table of contents
+    </div>
+    <ul v-if="toc && toc.links">
+      <li v-for="link in toc.links" :key="link.text" class="group min-w-0">
+        <a
+          :href="`#${link.id}`"
+          class="block truncate py-1 text-sm lg:pr-3 text-primary-500 pl-4"
+        >
+          {{ link.text }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
