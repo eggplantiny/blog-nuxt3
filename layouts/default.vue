84c182f76b166b8225e42a3d7b85b8ba234a3377
eggplantiny <eggplantiny@gmail.com>
2022-07-15 14:38:45 +0900

    refector: sidebar 에 wrapping div 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,9 @@ import TableOfContents from '~/components/organisms/TableOfContents.vue'
     <!-- Body -->
     <div class="container !max-w-screen-2xl lg:px-6 mx-auto grid-layout">
       <!-- Sidebar -->
-      <Sidebar />
+      <div>
+        <Sidebar />
+      </div>
 
       <div class="grid px-6 md:px-8 py-8 lg:py-12 relative">
         <!-- Content -->
